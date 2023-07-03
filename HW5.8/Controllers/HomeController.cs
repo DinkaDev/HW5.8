eb321c4ba511b37f088819c9f5ebf0fd99f19539
Dinkalucky <burlaka.dina@gmail.com>
2023-07-03 18:02:34 +0300

    Добавьте файлы проекта.

---
+++ HW5.8/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using HW5._8.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HW5._8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Registration model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HW5.8/Models/Registration.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW5._8.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "The fild FirstName is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The fild LastName is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The fild Email is required")]
+        [EmailAddress(ErrorMessage = "Input the correct Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The fild DesiredDate is required")]
+        [FutureDate(ErrorMessage = "The fild DesiredDate should be in the future")]
+        [NotWeekend(ErrorMessage = "The fild DesiredDate should be not on the weekend")]
+        public DateTime DesiredDate { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date = (DateTime)value;
+            return date > DateTime.Now;
+        }
+    }
+
+    public class NotWeekendAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date = (DateTime)value;
+            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+        }
+    }
+}
